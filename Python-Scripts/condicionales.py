dd67de5882ee679b688129c5e834ab9e321c02a7
Guglio <ogugliotta@merlindataquality.com>
2021-01-29 18:49:15 -0300

    Condicionales en python

--- Python-Scripts/condicionales.py
+++ Python-Scripts/condicionales.py
@@ -5,10 +5,51 @@
 #     print('Es menor de edad')    
 
 
-numero  = int(input('Escribe un numero: '))
-if numero > 5:
-    print('Es mayor a cinco')
-elif numero == 5:
-    print('Es igual a cinco')
-else: 
-    print('Es menor a cinco')
+# numero  = int(input('Escribe un numero: '))
+# if numero > 5:
+#     print('Es mayor a cinco')
+# elif numero == 5:
+#     print('Es igual a cinco')
+# else: 
+#     print('Es menor a cinco')
+
+
+menu = """
+
+Bienvenido al conversor de monedas 
+
+1- Pesos colombianos
+2- Pesos argentinos
+3- Pesos mexicanos
+
+Elige una opcion. """
+
+opcion = input(menu)
+if opcion == '1':
+    pesos = input('Cuantos pesos colombianos quiere cambiar? ')
+    pesos = float(pesos)
+    valor_dolar  = 3875
+    dolares = pesos / float(valor_dolar)
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Usted tiene ' + dolares + ' dolares')
+    pass
+elif opcion == '2':
+    pesos = input('Cuantos pesos argentinos quiere cambiar? ')
+    pesos = float(pesos)
+    valor_dolar  = 65
+    dolares = pesos / float(valor_dolar)
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Usted tiene ' + dolares + ' dolares')
+elif opcion == '3':
+    pesos = input('Cuantos pesos mexicanos quiere cambiar? ')
+    pesos = float(pesos)
+    valor_dolar  = 24
+    dolares = pesos / float(valor_dolar)
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Usted tiene ' + dolares + ' dolares')
+else:
+    print('Ingresa una opcion valida')
+
